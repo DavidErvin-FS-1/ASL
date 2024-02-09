5b4008d73a64f096fe1f5746f731e0aee7fa061d
David Ervin <dnervin@student.fullsail.edu>
2024-02-09 14:19:54 -0500

    Fix shebang and date printing

--- exploring_docker/perl/hello.pl
+++ exploring_docker/perl/hello.pl
@@ -1,7 +1,11 @@
-#!/user/bin/perl
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use POSIX qw/strftime/;
 
 print "Hello, World!\n";
 
 my $date = strftime "%Y-%m-%d", localtime;
 
-print "Today is $date\n";s
+print "$date\n";
